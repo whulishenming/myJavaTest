5cdfe7a6bc2b85233930aef88e78f12460efb52b
李申明 <lishenming8@126.com>
2018-04-24 11:54:13 +0800

    地址经纬度转换

--- common/src/main/java/lsm/addressToLocation/Client.java
+++ common/src/main/java/lsm/addressToLocation/Client.java
@@ -17,17 +17,19 @@ public class Client {
 
     @Test
     public void translateAddressToLocation() throws Exception{
-        List<Map<String, String>> mapList = ReadExcelUtil.readExcel("d:\\Users\\shenmingli\\Desktop\\地址.xlsx");
+        List<Map<String, String>> mapList = ReadExcelUtil.readExcel("d:\\Users\\shenmingli\\Desktop\\公司.xlsx");
         Map<String, String> locationMap = new HashMap<>(10);
         for (Map<String, String> stringStringMap : mapList) {
             for (Map.Entry<String, String> stringStringEntry : stringStringMap.entrySet()) {
                 String response = HttpUtils.sendGet("http://api.map.baidu.com/geocoder/v2/", "address=" + stringStringEntry.getValue() + "&output=json&ak=kR4pItRapPGRhlU1q1N38FSBB5GnP0om");
                 BaiduResult baiduResult = JSONObject.parseObject(response, BaiduResult.class);
 
-                locationMap.put(stringStringEntry.getValue(), baiduResult.getResult().getLocation().toString());
+                if ("0".equals(baiduResult.getStatus())) {
+                    locationMap.put(stringStringEntry.getValue(), baiduResult.getResult().getLocation().toString());
+                }
             }
 
         }
-        WriteExcelUtils.writeExcel(locationMap, "d:\\Users\\shenmingli\\Desktop\\经纬度.xlsx", "经纬度");
+        WriteExcelUtils.writeExcel(locationMap, "d:\\Users\\shenmingli\\Desktop\\公司经纬度.xlsx", "经纬度");
     }
 }
--- common/src/main/java/lsm/util/WriteExcelUtils.java
+++ common/src/main/java/lsm/util/WriteExcelUtils.java
@@ -16,6 +16,8 @@ public class WriteExcelUtils {
 
     public static void writeExcel(Map<String, String> cellsMap, String filePath, String sheetName) throws Exception {
 
+        FileOutputStream fos = new FileOutputStream(new File(filePath));
+
         //创建工作簿
         XSSFWorkbook workbook = new XSSFWorkbook();
 
@@ -34,12 +36,10 @@ public class WriteExcelUtils {
             XSSFCell cellValue = row.createCell(1);
             //给单元格赋值
             cellValue.setCellValue(entry.getValue());
-
-            //创建输出流
-            FileOutputStream fos = new FileOutputStream(new File(filePath));
-            workbook.write(fos);
-            workbook.close();
-            fos.close();
         }
+        //创建输出流
+        workbook.write(fos);
+        workbook.close();
+        fos.close();
     }
 }
